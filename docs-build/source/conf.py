fb41d40043c29b03e9ef9ab918e1a9a560c95092
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2018-07-08 17:28:28 -0700

    Incremented docs version

--- docs-build/source/conf.py
+++ docs-build/source/conf.py
@@ -27,7 +27,7 @@ author = 'Raynaldo Rivera'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '3.1.14.1'
+release = '3.1.14.3'
 
 
 # -- General configuration ---------------------------------------------------

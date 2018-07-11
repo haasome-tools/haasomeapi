eced0b9f636e6a876e56fb68a95a280abae736f1
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2018-07-10 19:27:06 -0700

    Updated version number for pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='haasomeapi',
-      version='3.1.14.3',
+      version='3.1.14.4',
       description='Python module to interact with the Haasonline Local API',
       url='http://github.com/haasome-tools/haasomeapi',
       author='Haasome Tools',

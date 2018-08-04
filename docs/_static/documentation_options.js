86c188439beef17cd0404cda849b7e3556db8928
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2018-08-04 02:14:01 -0700

    Updated the html documentation for the project

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.14.13',
+    VERSION: '3.1.15.3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
